7be71dee30e5d4804624204d7debf3ea70fbd693
Celia Alonso <celiaag94@gmail.com>
2023-01-31 19:26:53 +0000

    I've added  buttons but they are still not working

---
+++ project-2/main.js
@@ -0,0 +1,12 @@
+
+<script> function myFunction() {
+    var x = document.getElementById("copy1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  </script>
+
+  

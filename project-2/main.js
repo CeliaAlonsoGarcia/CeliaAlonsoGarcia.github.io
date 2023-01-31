9700964037288787f2f1bb979f7280d4ed3d9faf
Celia Alonso <celiaag94@gmail.com>
2023-01-31 20:04:26 +0000

    my buttons work!

--- project-2/main.js
+++ project-2/main.js
@@ -1,5 +1,5 @@
 
-<script> function myFunction() {
+function myFunction1() {
     var x = document.getElementById("copy1");
     if (x.style.display === "none") {
       x.style.display = "block";
@@ -7,6 +7,39 @@
       x.style.display = "none";
     }
   }
-  </script>
 
+  const x =        
+  document.getElementById("show-buttons");
+if (x.style.display === "none") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+function myFunction2() {
+    var x = document.getElementById("copy2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
   
+  function myFunction3() {
+    var x = document.getElementById("copy3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+    
+function myFunction4() {
+    var x = document.getElementById("copy4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }

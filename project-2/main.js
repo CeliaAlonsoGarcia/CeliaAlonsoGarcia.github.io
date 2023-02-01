efc2ea1fc4e96d4109a8af8aafbabad8955aae03
Celia Alonso <celiaag94@gmail.com>
2023-02-01 13:52:39 +0000

    boxes project 2 opening & project 1 gallery set up

--- project-2/main.js
+++ project-2/main.js
@@ -1,45 +1,9 @@
 
-function myFunction1() {
-    var x = document.getElementById("copy1");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+  function toggleDisplay(id) {
+    var copy = document.getElementById(id);
+    if (copy.style.display === "none") {
+      copy.style.display = "block";
     } else {
-      x.style.display = "none";
-    }
-  }
-
-  const x =        
-  document.getElementById("show-buttons");
-if (x.style.display === "none") {
-    x.style.display = "block";
-} else {
-    x.style.display = "none";
-}
-}
-
-function myFunction2() {
-    var x = document.getElementById("copy2");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-  
-  function myFunction3() {
-    var x = document.getElementById("copy3");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-    
-function myFunction4() {
-    var x = document.getElementById("copy4");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
+      copy.style.display = "none";
     }
   }
